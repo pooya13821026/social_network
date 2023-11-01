2a86d269898bf705f3330a2d1674cb7c1b7924af
pooya <poria8210@gmail.com>
2023-11-01 22:45:42 +0330

    add redis_cache

--- myproject/blog/services/posts.py
+++ myproject/blog/services/posts.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.db import transaction
 from django.db.models import QuerySet
 from django.utils.text import slugify
@@ -18,17 +19,28 @@ def count_posts(*, user: BaseUser) -> int:
     return Post.objects.filter(author=user).count()
 
 
+def cache_profile(*, user: BaseUser) -> None:
+    profile = {
+        'posts_count': count_posts(user=user),
+        'subscriber_count': count_follower(user=user),
+        'subscription_count': count_follower(user=user),
+    }
+    cache.set(f'profile_{user}', profile, timeout=None)
+
+
 def subscriber(*, user: BaseUser, email: str) -> QuerySet[Subscription]:
     target = BaseUser.objects.get(email=email)
     sub = Subscription(subscriber=user, target=target)
     sub.full_clean()
     sub.save()
+    cache_profile(user=user)
     return sub
 
 
 def unsubscriber(*, user: BaseUser, email: str) -> dict:
     target = BaseUser.objects.get(email=email)
     Subscription.objects.get(subscriber=user, target=target).delete()
+    cache_profile(user=user)
 
 
 @transaction.atomic
@@ -36,4 +48,5 @@ def create_post(*, user: BaseUser, title: str, content: str) -> QuerySet[Post]:
     post = Post.objects.create(
         author=user, title=title, content=content, slug=slugify(title)
     )
+    cache_profile(user=user)
     return post
